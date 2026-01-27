6c8fb3eb436482d446d27cda735c50705eff350f
CodePicsel <bankarshashank9@gmail.com>
2026-01-27 19:07:24 +0530

    db changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './Components/login'
+import LoginWithOtp from './Components/LoginO'
 import Register from './Components/Register'
 
 function App() {
